1f2ec4bc22e4817f774a21fd59301e376e460a48
kouinkouin <me@kouinkouin.com>
2015-08-06 00:37:42 +0200

    Declare some others displays

--- js/timestamp.js
+++ js/timestamp.js
@@ -44,6 +44,24 @@ Kouinkouin.Counter = (function() {
 		return formatDate(days, hours, minutes, seconds);
 	};
 
+    var displays = [
+        function (timestamp) {
+            return countEpisodes(timestamp, 55 * 60, 'Inspector Derrick');
+        },
+        function (timestamp) {
+            return countEpisodes(timestamp, 5 * 60, 'the Barbapapa');
+        },
+        daysBetween
+    ];
+
+    var countEpisodes = function( timestamp, episodeDuration, movieName) {
+        var episodesNumber = Math.floor(timestamp / episodeDuration);
+        var formattedDate = daysBetween(timestamp % episodeDuration);
+
+        return episodesNumber + ' ' + (episodesNumber > 1 ? 'episodes' : 'episode') + ' of ' + movieName
+            + (formattedDate == '' ? '' : '<br /> and ' + formattedDate);
+    };
+
 	var resultJson = function(data){
 		diffTimestamp = data.endOfSuffering - data.timestamp;
 		fillIn(daysBetween(diffTimestamp));

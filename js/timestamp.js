39fd6520df7924665ac27a89eea3259368726583
LGnap <lgnap@helpcomputer.org>
2015-08-07 16:26:22 +0200

    fix issues with tab & spaces => all is 4 spaces

--- js/timestamp.js
+++ js/timestamp.js
@@ -1,22 +1,22 @@
 var Kouinkouin = Kouinkouin || {};
 
 Kouinkouin.Counter = (function() {
-	var exports = {},
-		diffTimestamp,
-		domNode,
+    var exports = {},
+        diffTimestamp,
+        domNode,
         urlService,
         display;
 
-	exports.init = function(params){
-		domNode = params.domNode;
+    exports.init = function(params){
+        domNode = params.domNode;
         urlService = params.url;
-		getJson();
-		setInterval(getJson, 60 * 1000);
-		setInterval(renewTimestamp, 1000);
+        getJson();
+        setInterval(getJson, 60 * 1000);
+        setInterval(renewTimestamp, 1000);
         display = displays[Math.floor(Math.random() * displays.length)];
-	};
+    };
 
-	function formatDate(days, hours, minutes, seconds) {
+    function formatDate(days, hours, minutes, seconds) {
         var formattedDate = '';
         if (formattedDate != '' || days > 0) {
             formattedDate += days + ' ' + (days > 1 ? 'days' : 'day') + ', ';
@@ -31,20 +31,20 @@ Kouinkouin.Counter = (function() {
             formattedDate += seconds + ' ' + (seconds > 1 ? 'seconds' : 'second');
         }
         return formattedDate;
-	}
+    }
 
-	var daysBetween = function( timestamp ) {
+    var daysBetween = function( timestamp ) {
 
-		var seconds = Math.floor(timestamp % 60);
-		timestamp = timestamp/60;
-		var minutes = Math.floor(timestamp % 60);
-		timestamp = timestamp/60;
-		var hours = Math.floor(timestamp % 24);
+        var seconds = Math.floor(timestamp % 60);
+        timestamp = timestamp/60;
+        var minutes = Math.floor(timestamp % 60);
+        timestamp = timestamp/60;
+        var hours = Math.floor(timestamp % 24);
 
-		var days = Math.floor(timestamp/24);
+        var days = Math.floor(timestamp/24);
 
-		return formatDate(days, hours, minutes, seconds);
-	};
+        return formatDate(days, hours, minutes, seconds);
+    };
 
     var displays = [
         function (timestamp) {
@@ -64,16 +64,16 @@ Kouinkouin.Counter = (function() {
             + (formattedDate == '' ? '' : '<br /> and ' + formattedDate);
     };
 
-	var resultJson = function(data){
-		diffTimestamp = data.endOfSuffering - data.timestamp;
-		fillIn(display(diffTimestamp));
-	};
+    var resultJson = function(data){
+        diffTimestamp = data.endOfSuffering - data.timestamp;
+        fillIn(display(diffTimestamp));
+    };
 
-	var fillIn = function(data){
-		domNode.innerHTML = data;
-	};
+    var fillIn = function(data){
+        domNode.innerHTML = data;
+    };
 
-	var getJson = function() {
+    var getJson = function() {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -83,12 +83,12 @@ Kouinkouin.Counter = (function() {
         };
         xmlhttp.open("GET", urlService, true);
         xmlhttp.send();
-	};
+    };
 
-	var renewTimestamp = function(){
-		diffTimestamp--;
-		fillIn(display(diffTimestamp))
-	};
+    var renewTimestamp = function(){
+        diffTimestamp--;
+        fillIn(display(diffTimestamp))
+    };
 
-	return exports;
+    return exports;
 })();
